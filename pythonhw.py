34984af0ba1aeec4965b6144b73c37cdf0953ae7
joshua weng <joshua.wengh@gmail.com>
2022-09-15 22:49:36 +0800

    Completed

--- pythonhw.py
+++ pythonhw.py
@@ -24,12 +24,63 @@ request = {
 }
 
 
-def processRequest(request):
-    # Your code here
-    return
+try:
+        
+    def processRequest(request):
+        # Your code here
+        totalCost = 0
+        tickets = {}
+        successfulApplicants = list()
+        bannedApplications = list()
+        big_dict = {}
+        big_dict["successfulApplicants"] = successfulApplicants
+        big_dict["banneedApplicants"] = bannedApplications
+    
+        for name in request["applicants"]:
+            if name not in bannedVisitors:
+                successfulApplicants.append(name)
+            else:
+                bannedApplications.append(name)   
+        #print(successfulApplicants)
+        #print(bannedApplications)
+        
+        for name in request["applicants"]:
+            if name in memberStatus:
+                if memberStatus[name]:
+                    totalCost += 3.50
+                else:
+                    totalCost += 5              
+            else:
+                totalCost += 5
+        big_dict["totalCost"] = totalCost
+        
+        big_dict["tickets"] = list()
+        for name in request["applicants"]:
+            if name in memberStatus:
+                if memberStatus[name]:
+                    tickets["name"] = name
+                    tickets["membershipStatus"] = True
+                    tickets["price"] = 3.50
+                    big_dict["tickets"].append(tickets.copy())
+                else:
+                    tickets["name"] = 5
+                    tickets["membershipStatus"] = False
+                    tickets["price"] = 5
+                    big_dict["tickets"].append(tickets.copy())               
+            else:
+                tickets["name"] = name
+                tickets["membershipStatus"] = False
+                tickets["price"] = 5
+                big_dict["tickets"].append(tickets.copy())
+        #print(totalCost)
+        #print(big_dict)
+        return(big_dict)
+                
 
+    print(processRequest(request))
 
-print(processRequest(request))
+except:
+    print("No Applicants")
 
 # {
 #   successfulApplicants:
